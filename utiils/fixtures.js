6046a043631490819b194d7c3616602799455815
marcelo <marcheloortuo@gmail.com>
2025-10-03 20:20:15 -0400

    creación login page

---
+++ utiils/fixtures.js
@@ -0,0 +1,23 @@
+// fixtures/fixtures.js
+import { test as base } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const test = base.extend({
+  loginFixture: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+
+    // ir al login
+    await loginPage.gotoLogin();
+
+    // loguearse con credenciales de .env
+    await loginPage.login(process.env.TRELLO_EMAIL, process.env.TRELLO_PASSWORD);
+
+    // entregar la página ya logueada al test
+    await use(page);
+  },
+});
+
+export const expect = base.expect;
